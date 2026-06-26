6a7fe7c5dda137701ccf87a18ec3f7237503d7dc
lootcafecom <lootcafe.com@gmail.com>
2026-06-26 15:34:43 +0530

    Add files via upload

--- extractor/__init__.py
+++ extractor/__init__.py
@@ -1,17 +1,22 @@
 from .network_extractor import (
     extract_network_responses,
+    extract_network_responses_debug,
     find_products_in_json,
     extract_price_fields,
     extract_name_fields,
     extract_slug_fields,
     safe_float,
 )
+from .browser_pool import get_browser, capture_pharmacy
 
 __all__ = [
     "extract_network_responses",
+    "extract_network_responses_debug",
     "find_products_in_json",
     "extract_price_fields",
     "extract_name_fields",
     "extract_slug_fields",
     "safe_float",
+    "get_browser",
+    "capture_pharmacy",
 ]
